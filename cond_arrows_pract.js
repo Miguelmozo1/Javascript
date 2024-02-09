6aea96849ac1bdcbb823d428b6c5a1dbdd05329a
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2024-02-08 21:57:26 -0800

    Add files via upload

---
+++ cond_arrows_pract.js
@@ -0,0 +1,18 @@
+// problem 1
+const ofAge = age => age >= 18? "Go on in" : "Not old enough pal"
+// console.log(ofAge(17))
+
+
+// problem 2
+const isRaining = weather => weather == "yes"? "Get your rain jacket?" : "No rain on today's forecast!"
+// console.log(isRaining("no"))
+
+
+// problem 3
+const isEven = num => num % 2 == 0 ? "Number is even" : "Number is odd"
+// console.log(isEven(9))
+
+
+// problem 4
+const isGreater = (num1, num2) => num1 > num2 ? num1 + " is greater than " + num2 : num1 + " Is less than " + num2
+// console.log(isGreater(200,300))
