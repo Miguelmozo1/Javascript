3521d2efe71981239bd80fe5a5ec1566e0c45bfd
Miguel Mozo <142748517+Miguelmozo1@users.noreply.github.com>
2024-03-13 19:50:10 -0700

    Add files via upload

---
+++ fundamentals1.js
@@ -0,0 +1,128 @@
+    // 1
+// let myNumber = 42;
+// let myName = "Miguel Mozo"
+// let holder = ""
+
+// holder = myNumber;
+// myNumber = myName;
+// myName = holder;
+
+
+// console.log(`${myName} + ${myNumber}`)
+
+
+    // 2
+// function set2() {
+//     for(var i = -52; i <= 1066; i++) {
+//         console.log(i)
+//     }
+// }
+// set2()
+
+
+    // 3
+// const beCheerful = () => {
+//     let greeting = "good morning"
+//     return greeting
+// }
+// for (var i = 0; i <= 98; i++) {
+//     console.log(beCheerful() + " " + [i])
+// }
+
+
+    // 4
+// for(let i = -300; i <= 0; i++) {
+//     if (i === -6 || i === -3) {
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
+    // 5
+// let i = 2000
+// while ( i <= 5280) {
+//     console.log(i)
+//     i++
+// }
+
+
+    // 6
+    // my birthdate is 4/14
+// const birthday = (int, int2) => {
+//     if (int === 4 || int === 14) {
+//         if (int2 === 4 || int2 === 14) {
+//             console.log("How did you know?")
+//         }
+//     }
+//     else{
+//         console.log("Just another day...")
+//     }
+// }
+// birthday(4,14)
+
+
+    // 7
+// const leapYear = (year) => {
+//     if (year %4 == 0 && year %400 ==0 ) {
+//         console.log("It's a leap year!")
+//     }
+//     else{
+//         console.log("Not a leap year!")
+//     }
+// }
+// leapYear(2000)
+
+
+    // 8
+// var count = 0
+// for (var i = 512; i <=4096; i++) {
+//     if(i %5 == 0) {
+//         count++
+//         console.log(i)
+//     }
+// }
+// console.log(count + " integers divisible by 5")
+
+
+
+    // 9: Multiple of Six
+// var start = 6
+// while(start <= 6000) {
+//     if(start %6 ==0) {
+//         console.log(start)
+//     }
+//     start++
+// }
+
+
+
+    // 10
+// for(var i = 0; i <=100; i++) {
+//     console.log(i)
+//     if(i %10 == 0){
+//         console.log("Master")
+//         continue
+//     }
+//     if(i %5 ==0){
+//         console.log("Programming")
+//         continue
+//     }
+// }
+
+
+    // 11
+// const incoming = (inc) => {
+//     console.log(inc)
+// }
+// incoming("stage")
+
+    // 12
+// let i = 2016;
+// while(i >0){
+//     console.log(i)
+//     i -= 4
+// }
+
+
+    // 13
